6b4e2e5052d99a4e19e5db7808b5d8688a50b4f0
LAPTOP-TD2QPNFN\HP <yatinkapur@gmail.com>
2025-02-16 19:00:48 +0530

    Bird flap animation added

--- flappy-bird/Assets/Scripts/Player.cs
+++ flappy-bird/Assets/Scripts/Player.cs
@@ -1,15 +1,25 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     private Vector3 _direction;
+    private SpriteRenderer _spriteRenderer;
+    private int _spriteIndex;
 
+    public Sprite[] sprites;
     public float gravity = -8.0f;
-
     public float strength = 5f;
 
 
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimateBird),0.15f,0.15f);
+    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
@@ -29,5 +39,15 @@ public class Player : MonoBehaviour
         _direction.y += gravity * Time.deltaTime;  // Gravity => meter per sec sq
 
         transform.position += _direction * Time.deltaTime;
+    }
+
+    private void AnimateBird() {
+        _spriteIndex++;
+
+        if(_spriteIndex >= sprites.Length) {
+            _spriteIndex = 0;
+        }
+
+        _spriteRenderer.sprite = sprites[_spriteIndex];
     }
 }
